ea8f62d139b521eb6d2877a9200e0395027f1e8a
Diptangshu Ghosh <dippghosh7@gmail.com>
2024-09-03 17:01:24 +0530

    successful ai response. needs refactoring

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,7 +1,15 @@
 import { db } from "@/db";
+import { pinecone } from "@/lib/pinecone";
 import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import {
+  ChatGoogleGenerativeAI,
+  GoogleGenerativeAIEmbeddings,
+} from "@langchain/google-genai";
+import { PineconeStore } from "@langchain/pinecone";
 import { NextRequest } from "next/server";
+import { streamText } from "ai";
+import { google } from "@ai-sdk/google";
 
 export const POST = async (req: NextRequest) => {
   const body = await req.json();
@@ -38,4 +46,72 @@ export const POST = async (req: NextRequest) => {
       fileId,
     },
   });
+
+  const embeddings = new GoogleGenerativeAIEmbeddings({
+    apiKey: process.env.GOOGLE_API_KEY,
+    model: "models/embedding-001",
+  });
+
+  const pineconeIndex = pinecone.index("pdfgpt");
+
+  const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+    pineconeIndex,
+    namespace: file.id,
+  });
+
+  const results = await vectorStore.similaritySearch(message, 4);
+
+  const prevMessages = await db.message.findMany({
+    where: {
+      fileId,
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+    take: 6,
+  });
+
+  const formattedPrevMessages = prevMessages.map((msg) => ({
+    role: msg.isUserMessage ? ("user" as const) : ("assistant" as const),
+    content: msg.text,
+  }));
+
+  const model = new ChatGoogleGenerativeAI({
+    apiKey: process.env.GOOGLE_API_KEY,
+    model: "gemini-pro",
+    temperature: 1.0, // Control the randomness of the output
+    streaming: true, // Enable streaming mode
+    topK: 40, // Maintain a high value for diversity
+    topP: 0.9, // Set a moderate value for probability concentration
+  });
+
+  // Convert the prompt template into a single string
+  const prompt = `
+  System message:
+  Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
+        
+  \n----------------\n
+  
+  PREVIOUS CONVERSATION:
+  ${formattedPrevMessages.map((message) => {
+    if (message.role === "user") return `User: ${message.content}\n`;
+    return `Assistant: ${message.content}\n`;
+  })}
+  
+  \n----------------\n
+  
+  CONTEXT:
+  ${results.map((r) => r.pageContent).join("\n\n")}
+  
+  USER INPUT: ${message}`;
+
+  const modelT = google("models/gemini-1.5-pro-latest");
+
+  const result = await streamText({
+    model: modelT,
+    prompt,
+  });
+
+  // Respond with a streaming response
+  return result.toAIStreamResponse();
 };
--- src/components/chat/ChatContext.tsx
+++ src/components/chat/ChatContext.tsx
@@ -54,6 +54,8 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
   return (
     <ChatContext.Provider
       value={{ addMessage, message, handleInputChange, isLoading }}
-    ></ChatContext.Provider>
+    >
+      {children}
+    </ChatContext.Provider>
   );
 };
