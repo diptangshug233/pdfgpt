fe57bbcc2cf906e7ce022382b914cc3d16e9a886
Diptangshu Ghosh <dippghosh7@gmail.com>
2024-09-05 16:03:56 +0530

    stripe integration and pricing page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,10 @@ export function convertToAscii(inputString: string) {
   const asciiString = inputString.replace(/[^\x00-\x7F]+/g, "");
   return asciiString;
 }
+
+export function absoluteUrl(path: string) {
+  if (typeof window !== "undefined") return path;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`;
+
+  return `http://localhost:${process.env.PORT ?? 3000}${path}`;
+}
